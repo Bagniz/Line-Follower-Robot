dfef8cf502e317e7fefcb52eff21c6f464ef4e35
Salim <salimkhengui@gmail.com>
2019-11-19 18:34:20 +0100

    Mise a jour Detecteur RGB

--- RobotMindStorm/src/colorRecognition/ColorRecognition.java
+++ RobotMindStorm/src/colorRecognition/ColorRecognition.java
@@ -3,26 +3,46 @@ package colorRecognition;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
 import lejos.remote.ev3.RMISampleProvider;
 import lejos.remote.ev3.RemoteEV3;
-import lejos.utility.Delay;
 
 public class ColorRecognition
 {
+	static ArrayList<float[]> colorsLearned;
+	
+	static double getDistance(float[] captured, float[] had)
+	{
+		double x = Math.pow(captured[0] - had[0], 2);
+		double y = Math.pow(captured[1] - had[1], 2);
+		double z = Math.pow(captured[2] - had[2], 2);
+		
+		double distance = Math.sqrt(x+y+z);
+		return distance;
+	}
+	
 	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException
 	{
 		RemoteEV3 ev3 = new RemoteEV3("10.0.1.1");
 		RMISampleProvider sampleProvider = ev3.createSampleProvider("S4", "lejos.hardware.sensor.EV3ColorSensor", "RGB");
-		
-		float[] samples = new float[3];
-		
-		while(Button.ENTER.isUp())
+		float[] sample = new float[3];
+		LCD.clear();
+		while(Button.LEFT.isUp())
 		{
-			samples = sampleProvider.fetchSample();
-			System.out.println(samples[0] + " / " + samples[1] + " / " + samples[2]);
-			Delay.msDelay(100);	
+			LCD.drawString("Apprentissage", 0, 0);
+			LCD.drawString("Press Enter To learn new color", 0, 1);
+			Button.ENTER.waitForPressAndRelease();
+			sample = sampleProvider.fetchSample();
+			sample[0] = sample[0]*256f;
+			sample[1] = sample[1]*256f;
+			sample[2] = sample[2]*256f;
+			colorsLearned.add(sample);
+			LCD.drawString("Color Learned Red: " , 0, 2);
+			LCD.drawString("Press Right to learn another color", 0, 3);
+			Button.RIGHT.waitForPress();
 		}
 		sampleProvider.close();
 	}
